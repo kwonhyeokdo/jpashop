16b61bde73dba69dcb85b57abb8cc267eb9c7023
jpaPractice <jpaPractice>
2024-01-04 17:37:09 +0900

    Entity 완성, 회원 기능 구현

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 회원 이름 중복 체크
+    private void validateDuplicateMember(Member member) {
+        if(!memberRepository.findByName(member.getName()).isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    @Transactional
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    // 회원 단건 조회
+    @Transactional
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
